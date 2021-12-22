e71a30b9b938843447806d18644dd671ec3f149e
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-12-22 10:07:19 +0400

    task02

---
+++ app.py
@@ -0,0 +1,41 @@
+# Xətti alqoritm
+
+# 1. Düzbucaqlı üçbucaq katetlərinin qiymətlərini klaviaturadan ayrı sətirdə yazmaqla
+# daxil edin. Üçbucağın sahəsini hesablayan proqram yazın.
+
+# a = int(input('a: '))
+# b = int(input('b: '))
+
+
+# s = (a*b)/2
+
+# print(s)
+
+# 2. Üçrəqəmli natural ədəd verilib. Bu ədədin rəqəmləri arasına boşluq simvolu qoymaqla
+# çıxışa verin.
+
+# a = int(input('a: '))
+# b = int(input('b: '))
+# c = int(input('c: '))
+
+# print(f'{a} {b} {c}')
+
+# 1. Tam ədədi verilmişdir. Onun cüt olduğunu müəyyən edin.
+
+# sayi = int(input('sayi: '))
+# if sayi>0:
+#     if sayi % 2 ==0:
+#         print('cut ededdir.')
+#     else:
+#         print('tek edidir.')
+# else:
+#     print('eror')
+
+
+# 2. x, y həqiqi ədədlər verilmişdir. Bu ədədlərin ən böyüyünü (maksimal olanı) tapın.
+
+x= int(input('x: '))
+y= int(input('y: '))
+
+if x>0 and y>0:
+    print(max(x,y))
