c9a4fcb8e7a30200bf4befd2227d67dc730bfd82
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2021-12-26 22:28:16 +0400

    while

--- app.py
+++ app.py
@@ -20,7 +20,7 @@
 
 # print(f'{a} {b} {c}')
 
-# 1. Tam ədədi verilmişdir. Onun cüt olduğunu müəyyən edin.
+# 3. Tam ədədi verilmişdir. Onun cüt olduğunu müəyyən edin.
 
 # sayi = int(input('sayi: '))
 # if sayi>0:
@@ -32,10 +32,41 @@
 #     print('eror')
 
 
-# 2. x, y həqiqi ədədlər verilmişdir. Bu ədədlərin ən böyüyünü (maksimal olanı) tapın.
+# 4. x, y həqiqi ədədlər verilmişdir. Bu ədədlərin ən böyüyünü (maksimal olanı) tapın.
 
-x= int(input('x: '))
-y= int(input('y: '))
+# x= int(input('x: '))
+# y= int(input('y: '))
 
-if x>0 and y>0:
-    print(max(x,y))
+# if x>0 and y>0:
+#     print(max(x,y))
+
+# 5. 1-den 100e qeder ededlerin icinde 7e bolunenlerin cemini hesablayin funksiya yazmaq.
+# a = int(input('7e bolunen eded: '))
+# mylist = 0
+for i in range(1,101):
+    if i%7==0:
+        print('7e bolunen ededler')
+    else:
+        print('eror')
+
+        
+# a = [1,2,46,85,96]
+# mylist = 0
+# for i in a:
+#     if mylist<i:
+#         mylist = i
+# print(mylist)  
+      
+      
+# numbers = [2,2,4,3,6,9,1,5,1]
+
+# index = 0
+# listen = len(numbers)
+
+# while listen > index:
+#     if numbers[index] > 3:
+#         numbers.pop(index)
+#         listen -=1
+#         continue
+#     index +=1
+# print(numbers)
